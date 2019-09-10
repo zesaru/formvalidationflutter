ae558243f3a6cdd2a8c7e2d380a279f4980b7d64
Cesar Murillo <namiayumis@gmail.com>
2019-09-10 22:21:31 +0900

    pantalla detalle producto

--- lib/src/pages/producto_page.dart
+++ lib/src/pages/producto_page.dart
@@ -17,6 +17,49 @@ class ProductoPage extends StatelessWidget {
           ),
         ],
       ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.all(15.0),
+          child: Form(
+            child: Column(
+              children: <Widget>[
+                _crearNombre(),
+                _crearPrecio(),
+                _crearBoton(),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _crearNombre() {
+    return TextFormField(
+      textCapitalization: TextCapitalization.sentences,
+      decoration: InputDecoration(
+        labelText: 'Producto',
+      ),
+    );
+  }
+
+  Widget _crearPrecio() {
+    return TextFormField(
+      keyboardType: TextInputType.numberWithOptions(decimal: true),
+      decoration: InputDecoration(
+        labelText: 'Precio',
+      ),
+    );
+  }
+
+  Widget _crearBoton() {
+    return RaisedButton.icon(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20.0)),
+      color: Colors.deepPurple,
+      textColor: Colors.white,
+      label: Text('Guardar'),
+      icon: Icon(Icons.save),
+      onPressed: () {},
     );
   }
 }
